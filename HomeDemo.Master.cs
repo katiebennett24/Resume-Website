b20d8107109063cc15f2e1d40ba19694faa00f01
HomelyLemuyr <99858044+HomelyLemuyr@users.noreply.github.com>
2023-04-01 17:41:06 -0500

    Pages
    Added all the pages. 
    There is no VS packages, css or js files.

---
+++ HomeDemo.Master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test
+{
+     public partial class HomeDemo : System.Web.UI.MasterPage
+     {
+          protected void Page_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          protected void btnAccountCreation_Click(object sender, EventArgs e)
+          {
+
+            Response.Redirect("CreateAccount.aspx", false);
+          }
+
+          protected void btnLogin_Click(object sender, EventArgs e)
+          {
+            Response.Redirect("UserLogin.aspx", false);
+          }
+
+          protected void btnLogOut_Click(object sender, EventArgs e)
+          {
+            
+          }
+
+          protected void btnUserDashboard_Click(object sender, EventArgs e)
+          {
+            Response.Redirect("UserDashBoard.aspx", false);
+          }
+    }
+}
